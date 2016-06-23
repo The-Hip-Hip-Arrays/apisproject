7845bfcd36e0aec5a65c5c4272753ffcc9e7cc36
mcelearr <rorymcelearney@gmail.com>
2016-06-23 11:27:31 +0100

    changes caption position to right of each photo

---
+++ js/fairy-content.js
@@ -0,0 +1,13 @@
+var start = [
+  "You shall go to the ball but what to wear? Let me, your Fairy Godmother decide.",
+  "Well look who it is, my favourite human. Back for more fashion advice from your Fairy Godmother."
+]
+
+var comments = [
+  "While we're chatting millions around the world languish in abject poverty but let's talk about dresses yay.",
+  "There is literally nothing I would rather do with my time than help you choose clothes.",
+  "Not sure that works as an ensemble. The human in the middle rather ruins it for me.",
+  "That top doesn't suit you. Try harder.",
+  "I love what your doing with your hair by the way. Which charity are you raising money for?",
+  "I'm pretty much infitely powerful by the way. You know, just in case you want me to cure cancer after we're done here but it's cool let's do your thing first."
+]
