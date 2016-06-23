4cc5f440f6748a4be37dbd49287a3d8a6503687b
Shadows666 <lifeloveregretsxe@gmail.com>
2016-06-23 18:09:13 +0100

    wrote another test

--- js/test.js
+++ js/test.js
@@ -10,3 +10,7 @@ test('testing type of array element is set up', function(assert) {
   assert.ok(typeof womensTrousersArr === 'object', 'trousers array is set up as an array object')
   assert.ok(typeof womensShoesArr === 'object', 'shoes array is set up as an array object')
 });
+
+test('check to see if fairyhead button exists', function(assert){
+  assert.ok(document.getElementById('fairyhead'), 'fairyhead button object that represents the DOM element exists')
+});
