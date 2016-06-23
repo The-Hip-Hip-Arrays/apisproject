94034baffba90e9d663633cab77176c17d14adac
Shadows666 <lifeloveregretsxe@gmail.com>
2016-06-23 14:23:09 +0100

    Set up buttons and add event listener functions

--- js/app.js
+++ js/app.js
@@ -10,81 +10,78 @@ var womensShoes = "?category=womens-shoes&pageSize=200"
 var womensHats = "?category=womens-hats-caps&pageSize=200"
 
 function randomHatGenerator() {
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4 && this.status === 200) {
-    var result = JSON.parse(this.response);
-    womensHatsArr.push(result);
-    var randomNumber = womensHatsArr[0].content[Math.floor(Math.random() * 201)];
-    document.getElementById('hats-img').src = randomNumber.media.images[0].mediumUrl;
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var result = JSON.parse(this.response);
+      womensHatsArr.push(result);
+      var randomNumber = womensHatsArr[0].content[Math.floor(Math.random() * womensHatsArr[0].content.length + 1)];
+      document.getElementById('hats-img').src = randomNumber.media.images[0].mediumUrl;
+    }
 });
 
-xhr.open("GET", "https://api.zalando.com/articles" + womensHats);
-xhr.setRequestHeader("accept-language", "en");
-xhr.send(data);
+  xhr.open("GET", "https://api.zalando.com/articles" + womensHats);
+  xhr.setRequestHeader("accept-language", "en");
+  xhr.send(data);
 }
 
 function randomTopGenerator() {
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4 && this.status === 200) {
-    var result = JSON.parse(this.response);
-    womensTopsArr.push(result);
-    var randomNumber = womensTopsArr[0].content[Math.floor(Math.random() * 201)];
-    document.getElementById('tops-img').src = randomNumber.media.images[0].mediumUrl;
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var result = JSON.parse(this.response);
+      womensTopsArr.push(result);
+      var randomNumber = womensTopsArr[0].content[Math.floor(Math.random() * womensTopsArr[0].content.length + 1)];
+      document.getElementById('tops-img').src = randomNumber.media.images[0].mediumUrl;
+    }
 });
 
-xhr.open("GET", "https://api.zalando.com/articles" + womensTops);
-xhr.setRequestHeader("accept-language", "en");
-xhr.send(data);
-
+  xhr.open("GET", "https://api.zalando.com/articles" + womensTops);
+  xhr.setRequestHeader("accept-language", "en");
+  xhr.send(data);
 }
 
 function randomTrousersGenerator() {
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4 && this.status === 200) {
-    var result = JSON.parse(this.response);
-    womensTrousersArr.push(result);
-    var randomNumber = womensTrousersArr[0].content[Math.floor(Math.random() * 201)];
-
-document.getElementById('trousers-img').src = randomNumber.media.images[0].mediumUrl;
-
-
-
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var result = JSON.parse(this.response);
+      womensTrousersArr.push(result);
+      var randomNumber = womensTrousersArr[0].content[Math.floor(Math.random() * womensTrousersArr[0].content.length + 1)];
+      document.getElementById('trousers-img').src = randomNumber.media.images[0].mediumUrl;
+    }
 });
 
-xhr.open("GET", "https://api.zalando.com/articles" + womensTrousers);
-xhr.setRequestHeader("accept-language", "en");
-xhr.send(data);
+  xhr.open("GET", "https://api.zalando.com/articles" + womensTrousers);
+  xhr.setRequestHeader("accept-language", "en");
+  xhr.send(data);
 }
 
 
 function randomShoesGenerator() {
-var xhr = new XMLHttpRequest();
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4 && this.status === 200) {
-    var result = JSON.parse(this.response);
-    womensShoesArr.push(result);
-    var randomNumber = womensShoesArr[0].content[Math.floor(Math.random() * 201)];
-    document.getElementById('shoes-img').src = randomNumber.media.images[0].mediumUrl;
-    console.log(randomNumber.name);
-
-
-}
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var result = JSON.parse(this.response);
+      womensShoesArr.push(result);
+      var randomNumber = womensShoesArr[0].content[Math.floor(Math.random() * womensShoesArr[0].content.length + 1)];
+      document.getElementById('shoes-img').src = randomNumber.media.images[0].mediumUrl;
+    }
 });
 
-
-
-xhr.open("GET", "https://api.zalando.com/articles" + womensShoes);
-xhr.setRequestHeader("accept-language", "en");
-xhr.send(data);
+  xhr.open("GET", "https://api.zalando.com/articles" + womensShoes);
+  xhr.setRequestHeader("accept-language", "en");
+  xhr.send(data);
 }
 
-randomHatGenerator();
-randomTopGenerator();
-randomTrousersGenerator();
-randomShoesGenerator();
+document.getElementById('fashionbutton').addEventListener("click", function(){
+  randomHatGenerator();
+  randomTopGenerator();
+  randomTrousersGenerator();
+  randomShoesGenerator();
+});
+
+document.getElementById('fairybutton').addEventListener("click", function(){
+  var randomComment = comments[Math.floor(Math.random() * comments.length)];
+  document.getElementById('fairy-text').textContent = randomComment;
+});
